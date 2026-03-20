0533592483dff6b0466e7f32a6cee64cfcd5f6d5
Sann's Makbook Pro <sannsmakbookpro@Sanns-MacBook-Pro.local>
2026-03-20 09:34:26 -0400

    add: postgresql initial migration

---
+++ prisma/migrations/20260320133337_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Token" (
+    "id" INTEGER NOT NULL,
+    "token" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Token_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Token_token_key" ON "Token"("token");
